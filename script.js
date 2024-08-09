b8e6734acf4ece43fa7f247b92778500790c2e2f
eser <volkaneserokan8@gmail.com>
2024-08-09 16:05:42 +0300

    giriş yap ekranı

---
+++ script.js
@@ -0,0 +1,10 @@
+const myBtn = document.querySelector(".myBtn")
+
+myBtn.addEventListener("click", () => {
+
+
+    // document.body.style.backgroundColor = "red"
+
+    // alert("asdkljasdj")
+
+})
