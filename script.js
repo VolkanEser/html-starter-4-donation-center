c8306778fdca84242c626f2533636fd04474f159
eser <volkaneserokan8@gmail.com>
2024-08-12 15:53:01 +0300

    hfxyhrgdfcgv

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-const myBtn = document.querySelector(".myBtn")
+// const myBtn = document.querySelector(".myBtn")
 
-myBtn.addEventListener("click", () => {
+// myBtn.addEventListener("click", () => {
 
 
-    // document.body.style.backgroundColor = "red"
+//     // document.body.style.backgroundColor = "red"
 
-    // alert("asdkljasdj")
+//     // alert("asdkljasdj")
 
-})
+// })
+
+/*javascripte degişken tanımlamaları assagıdaki gibidır 
+ilk kelime kücuk harfle baslar ikinci lelimenın ılk harfi buyuk harfle baslar 
+ornek:
+cengizKurtoglu
+volkanEser 
+
+*/
+const toggleBtn = document.querySelector(".toggle-btn");
+toggleBtn.
+
+
+// ! butons
+
+const navbar = document.querySelector(".navbar");
